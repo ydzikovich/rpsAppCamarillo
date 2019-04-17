2ee8e09e6abc2d4962fe02287cc3ad9b58aa57f1
ydzikovich <ydzikovich@pivotal.io>
2019-04-17 00:24:07 -0700

    - Added a failing UI test - Fixed babelify version

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,5 +1,27 @@
-describe("foo", function () {
-    it("bars", function () {
-        expect(true).toBe(false)
+const React = require("react")
+const ReactDOM = require("react-dom")
+
+describe("play form", function () {
+    describe("when the play use case tells the UI that the input is invalid", function () {
+        it("tells the user that their input is invalid", function () {
+            let domFixture = document.createElement("div")
+            domFixture.id = "hello test world!"
+            document.querySelector("body").appendChild(domFixture)
+
+            const requests = {
+                play: function(p1, p2, ui){
+                    ui.invalid()
+                }
+            }
+
+            ReactDOM.render(
+            <RPSApp requests={requests}/>,
+            domFixture
+        )
+
+            document.querySelector("button").click()
+
+            expect(domFixture.innerText).toContain("INVALID")
+        })
     })
 })
