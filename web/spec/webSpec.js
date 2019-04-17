b2772f592599ef982186150caea84f117d9ac67f
ydzikovich <ydzikovich@pivotal.io>
2019-04-17 00:43:46 -0700

    - Added skeleton for the React app

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -1,11 +1,24 @@
 const React = require("react")
 const ReactDOM = require("react-dom")
+const ReactTestUtils = require("react-dom/test-utils")
+
+// React cheat sheet https://gist.github.com/moonmaster9000/941b619d6b25cc740aad5f7e926a5150
+
+class PlayForm extends React.Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return <div></div>
+    }
+}
 
 describe("play form", function () {
     describe("when the play use case tells the UI that the input is invalid", function () {
         it("tells the user that their input is invalid", function () {
             let domFixture = document.createElement("div")
-            domFixture.id = "hello test world!"
+            domFixture.id = "hello"
             document.querySelector("body").appendChild(domFixture)
 
             const requests = {
