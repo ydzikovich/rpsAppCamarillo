a0ed3c29346b47fc4d6f4ee35acffcbf32b291c4
ydzikovich <ydzikovich@pivotal.io>
2019-04-18 09:39:02 -0700

    added invalid and tie front end tests

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -7,15 +7,50 @@ const ReactTestUtils = require("react-dom/test-utils")
 class PlayForm extends React.Component {
     constructor(){
         super()
+        this.state = {message: ""}
+    }
+
+    handlePlay(){
+        this.setState({message: "Invalid"})
     }
 
     render(){
-        return <div></div>
+        return <div>
+            <div>{this.state.message}</div>
+            <button onClick={this.handlePlay.bind(this)}>Play</button>
+        </div>
     }
 }
 
 describe("play form", function () {
-    describe("when the play use case tells the UI that the input is invalid", function () {
+    describe("when rps module tells the UI that the result is tie", function () {
+        it("tells the user that it's a tie", function () {
+            let domFixture = document.createElement("div")
+            domFixture.id = "hello"
+            document.querySelector("body").appendChild(domFixture)
+
+            const requests = {
+                play: function(p1, p2, ui){
+                    ui.tie()
+                }
+            }
+
+            ReactDOM.render(
+            <PlayForm requests={requests}/>,
+            domFixture
+        )
+
+            expect(domFixture.innerText).not.toContain("Tie")
+            expect(domFixture.innerText).not.toContain("Invalid")
+
+            document.querySelector("button").click()
+
+            expect(domFixture.innerText).toContain("Tie")
+            expect(domFixture.innerText).not.toContain("Invalid")
+        })
+    })
+
+    describe("when rps module tells the UI that the input is invalid", function () {
         it("tells the user that their input is invalid", function () {
             let domFixture = document.createElement("div")
             domFixture.id = "hello"
@@ -28,9 +63,11 @@ describe("play form", function () {
             }
 
             ReactDOM.render(
-            <PlayForm requests={requests}/>,
-            domFixture
-        )
+                <PlayForm requests={requests}/>,
+                domFixture
+            )
+
+            expect(domFixture.innerText).not.toContain("Invalid")
 
             document.querySelector("button").click()
 
@@ -38,3 +75,15 @@ describe("play form", function () {
         })
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
